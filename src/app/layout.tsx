6ee027b48709e5537bc39d58c0d0398f2ce78d02
root <root@gustavo-dell.localdomain>
2025-03-24 02:31:17 -0300

    style: 💄 change theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className={`dark antialiased`}>
         <Header />
         {children}
       </body>
