e62fe3e1e1be1fc7b97d7f7821a15dca875f6ee4
root <gustavob.paula@gmail.com>
2025-03-26 12:23:22 -0300

    docs: ✏️ add doc

--- src/app/characters/[id]/InitializerCharacterStore.tsx
+++ src/app/characters/[id]/InitializerCharacterStore.tsx
@@ -3,6 +3,21 @@
 import { useCharacterStore, CharactersStore } from '@/store'
 import { useRef } from 'react'
 
+/**
+ * InitializerCharacterStore
+ *
+ * This component ensures the rehydration of the state from the server to the client.
+ * It initializes the character state in the client-side store with the data provided
+ * from the server-side rendering.
+ *
+ * @param {Pick<CharactersStore, 'character'>} props - The character data to initialize the store with.
+ * @returns {null} This component does not render any UI.
+ *
+ * Usage:
+ * ```tsx
+ * <InitializerCharacterStore character={character} />
+ * ```
+ */
 export const InitializerCharacterStore = ({ character }: Pick<CharactersStore, 'character'>) => {
   const isInitialized = useRef(false)
 
--- src/app/planets/[id]/InitializerPlanetStore.tsx
+++ src/app/planets/[id]/InitializerPlanetStore.tsx
@@ -3,6 +3,21 @@
 import { usePlanetStore, PlanetsStore } from '@/store'
 import { useRef } from 'react'
 
+/**
+ * InitializerCharacterStore
+ *
+ * This component ensures the rehydration of the state from the server to the client.
+ * It initializes the character state in the client-side store with the data provided
+ * from the server-side rendering.
+ *
+ * @param {Pick<CharactersStore, 'character'>} props - The character data to initialize the store with.
+ * @returns {null} This component does not render any UI.
+ *
+ * Usage:
+ * ```tsx
+ * <InitializerCharacterStore character={character} />
+ * ```
+ */
 export const InitializerPlanetStore = ({ planet }: Pick<PlanetsStore, 'planet'>) => {
   const isInitialized = useRef(false)
 
